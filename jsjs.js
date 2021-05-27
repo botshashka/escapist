043e71ffbd0c9c344fd2a17822a83b1bece6f7ad
botshashka <davidjobbagy07@gmail.com>
2021-05-28 01:56:18 +0200

    main page

--- jsjs.js
+++ jsjs.js
@@ -20,8 +20,18 @@ var x = setInterval(function() {
     }
 }, 1000);
 
-$('input').on('change', function () {
-    $('body').toggleClass('blue');
-});
+var slideIndex = 0;
+carousel();
 
-
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 4000); // Change image every 4 seconds
+}
+// From w3schools.com
